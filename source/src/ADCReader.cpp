596820496bcb2ba61310163250b47953c1dbc859
Timo Wischer <twischer@freenet.de>
2020-11-02 18:26:26 +0100

    adc: Set offsets
    Signed-off-by: Timo Wischer <twischer@freenet.de>

--- source/src/ADCReader.cpp
+++ source/src/ADCReader.cpp
@@ -2,16 +2,16 @@
 
 const ADCReader::adc2unit_t ADCReader::adc2unit[ADC_MAX] = {
 	[ADC_CURRENT_IN] = {
-		.offset = 0, // TODO
-		.gain = 1.0,
+		.offset = -733,
+		.gain = 0.04245421,
 	},
 	[ADC_VOLTAGE_IN] = {
-		.offset = 0, // TODO
-		.gain = 1.0,
+		.offset = -4,
+		.gain = 0.02524754,
 	},
 	[ADC_VOLTAGE_OUT] = {
-		.offset = 0, // TODO
-		.gain = 1.0,
+		.offset = -5,
+		.gain = 0.05386298,
 	},
 };
 
@@ -22,7 +22,10 @@ ADC_MODE(ADC_TOUT_3V3)
  */
 bool ADCReader::update()
 {
-	const uint16_t value = analogRead(A0) + adc2unit[nextChannel].offset;
+	const uint16_t adc = analogRead(A0);
+	const int16_t offset = adc2unit[nextChannel].offset;
+
+	const uint16_t value = (adc > -offset) ? (adc + offset) : 0;
 	values[nextChannel].update(value);
 
 	nextChannel++;
