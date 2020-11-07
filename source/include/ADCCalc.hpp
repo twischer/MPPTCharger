3c08694767e8d73418024dfb4028cbfda0df8695
Timo Wischer <twischer@freenet.de>
2020-11-07 16:54:05 +0100

    adcclacl: caluclate everything in float
    Signed-off-by: Timo Wischer <twischer@freenet.de>

--- source/include/ADCCalc.hpp
+++ source/include/ADCCalc.hpp
@@ -9,17 +9,14 @@ enum ADC_CALC {
 
 class ADCCalc : public ADCReader {
 private:
-	static const float powerGain;
-	static const uint32_t MS_PER_H = 60 * 60 * 1000;
-	static const float energyGain;
-
+	static constexpr float MS_PER_H = 60 * 60 * 1000;
 
 	unsigned long lastUpdateTime;
-	uint32_t power;
+	float power;
 	float energy;
 
     public:
-	ADCCalc() : lastUpdateTime(0), power(0), energy(0.0)
+	ADCCalc() : lastUpdateTime(0), power(0.0), energy(0.0)
 	{
 	}
 
@@ -33,35 +30,28 @@ private:
 		const unsigned long now = millis();
 		if (lastUpdateTime > 0) {
 			const uint64_t diffTime = now - lastUpdateTime;
-			energy += static_cast<float>(power) * diffTime;
+			energy += power * diffTime / MS_PER_H;
 		}
 
 		lastUpdateTime = now;
 		return true;
 	}
 
-	uint32_t get(const uint8_t channel) {
+	float get(const uint8_t channel) {
 		if (channel < ADC_MAX)
 			return ADCReader::get(channel);
 		else if (channel == ADC_POWER_IN) {
+			/* in W */
 			return power;
 		} else if (channel == ADC_ENERGY_IN) {
+			/* in Wh */
 			return energy;
 		} else
 			return 0;
 	}
 
 	float getInUnit(const uint8_t channel) {
-		if (channel < ADC_MAX)
-			return ADCReader::getInUnit(channel);
-		else if (channel == ADC_POWER_IN) {
-			/* in W */
-			return power * powerGain;
-		} else if (channel == ADC_ENERGY_IN) {
-			/* in Wh */
-			return energy * energyGain;
-		} else
-			return 0;
+		return get(channel);
 	}
 };
 
--- source/src/ADCCalc.cpp
+++ source/src/ADCCalc.cpp
@@ -1,6 +1,2 @@
 #include "ADCCalc.hpp"
 
-const float ADCCalc::powerGain = adc2unit[ADC_VOLTAGE_IN].gain *
-	adc2unit[ADC_CURRENT_IN].gain;
-const float ADCCalc::energyGain = powerGain / MS_PER_H;
-
