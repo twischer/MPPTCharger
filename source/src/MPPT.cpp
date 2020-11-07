6e89064ea4f8b03ddca25bd14994b241c32faae2
Timo Wischer <twischer@freenet.de>
2020-11-07 15:49:49 +0100

    mppt: remeber maxima until smaller threshold
    Signed-off-by: Timo Wischer <twischer@freenet.de>

--- source/src/MPPT.cpp
+++ source/src/MPPT.cpp
@@ -6,25 +6,33 @@ void MPPT::update(const uint32_t voltage, const uint32_t power)
 	// TODO move to class
 	static bool increase = true;
 
-	if (pwm < PWM_UPDATE_DIFF)
-		increase = true;
 	// TODO use voltage value e.g. 10 V check what is reasonabel for TL494
 	// Use only bool input e.g. minVoltageExceeded
-	else if (voltage < 600 || pwm > (maxPWM - PWM_UPDATE_DIFF))
+	if (voltage < 600 || pwm > (maxPWM - PWM_UPDATE_DIFF))
 		increase = false;
+	else if (pwm < PWM_UPDATE_DIFF)
+		increase = true;
 	/* compensate ADC uncertainty */
-	else if ( (power + 10) < lastPower )
+	// circa 1W bzw vielleicht prozentual
+	else if ( (power + 10) < lastPower) {
 		increase = !increase;
+		lastPower = power;
+	} else if (power > lastPower)
+		lastPower = power;
 
 	if (increase)
 		pwm += PWM_UPDATE_DIFF;
-	else
-		pwm -= PWM_UPDATE_DIFF;
+	else {
+		/* required in case voltage to small */
+		if (pwm <= PWM_UPDATE_DIFF)
+			pwm = 0;
+		else
+			pwm -= PWM_UPDATE_DIFF;
+	}
 
 	/* inverted logic */
 	sigmaDeltaWrite(SIGMA_DELTA_CHANNEL, maxPWM - pwm);
 
 	lastVoltage = voltage; // TODO Not required any longer
-	lastPower = power;
 }
 
