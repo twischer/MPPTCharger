1b5c241e3459a89d3ef7b19c9bd5345a77a84fa3
Timo Wischer <twischer@freenet.de>
2021-04-29 23:12:55 +0200

    fixup! Provide Arduino lib dependencies

--- source/lib/arduino/Print.h
+++ source/lib/arduino/Print.h
@@ -31,76 +31,83 @@
 #define OCT 8
 #define BIN 2
 
-class Print {
-    private:
-        int write_error;
-        size_t printNumber(unsigned long, uint8_t);
-        size_t printFloat(double, uint8_t);
-    protected:
-        void setWriteError(int err = 1) {
-            write_error = err;
-        }
-    public:
-        Print() :
-                write_error(0) {
-        }
+class Print
+{
+private:
+    int write_error;
+    size_t printNumber(unsigned long, uint8_t);
+    size_t printNumber(unsigned long long, uint8_t);
+    size_t printFloat(double, uint8_t);
+protected:
+    void setWriteError(int err = 1)
+    {
+        write_error = err;
+    }
+public:
+    Print() :
+        write_error(0)
+    {
+    }
+    virtual ~Print() {}
+    int getWriteError()
+    {
+        return write_error;
+    }
+    void clearWriteError()
+    {
+        setWriteError(0);
+    }
 
-        int getWriteError() {
-            return write_error;
+    virtual size_t write(uint8_t) = 0;
+    size_t write(const char *str)
+    {
+        if(str == NULL) {
+            return 0;
         }
-        void clearWriteError() {
-            setWriteError(0);
-        }
+        return write((const uint8_t *) str, strlen(str));
+    }
+    virtual size_t write(const uint8_t *buffer, size_t size);
+    size_t write(const char *buffer, size_t size)
+    {
+        return write((const uint8_t *) buffer, size);
+    }
 
-        virtual size_t write(uint8_t) = 0;
-        size_t write(const char *str) {
-            if(str == NULL)
-                return 0;
-            return write((const uint8_t *) str, strlen(str));
-        }
-        virtual size_t write(const uint8_t *buffer, size_t size);
-        size_t write(const char *buffer, size_t size) {
-            return write((const uint8_t *) buffer, size);
-        }
-        // These handle ambiguity for write(0) case, because (0) can be a pointer or an integer
-        inline size_t write(short t) { return write((uint8_t)t); }
-        inline size_t write(unsigned short t) { return write((uint8_t)t); }
-        inline size_t write(int t) { return write((uint8_t)t); }
-        inline size_t write(unsigned int t) { return write((uint8_t)t); }
-        inline size_t write(long t) { return write((uint8_t)t); }
-        inline size_t write(unsigned long t) { return write((uint8_t)t); }
-        // Enable write(char) to fall through to write(uint8_t)
-        inline size_t write(char c) { return write((uint8_t) c); }
-        inline size_t write(int8_t c) { return write((uint8_t) c); }
+    size_t printf(const char * format, ...)  __attribute__ ((format (printf, 2, 3)));
 
-        size_t printf(const char * format, ...)  __attribute__ ((format (printf, 2, 3)));
-        size_t printf_P(const char* format, ...) __attribute__((format(printf, 2, 3)));
-        size_t print(const __FlashStringHelper *);
-        size_t print(const String &);
-        size_t print(const char[]);
-        size_t print(char);
-        size_t print(unsigned char, int = DEC);
-        size_t print(int, int = DEC);
-        size_t print(unsigned int, int = DEC);
-        size_t print(long, int = DEC);
-        size_t print(unsigned long, int = DEC);
-        size_t print(double, int = 2);
-        size_t print(const Printable&);
+    // add availableForWrite to make compatible with Arduino Print.h
+    // default to zero, meaning "a single write may block"
+    // should be overriden by subclasses with buffering
+    virtual int availableForWrite() { return 0; }
+    size_t print(const __FlashStringHelper *);
+    size_t print(const String &);
+    size_t print(const char[]);
+    size_t print(char);
+    size_t print(unsigned char, int = DEC);
+    size_t print(int, int = DEC);
+    size_t print(unsigned int, int = DEC);
+    size_t print(long, int = DEC);
+    size_t print(unsigned long, int = DEC);
+    size_t print(long long, int = DEC);
+    size_t print(unsigned long long, int = DEC);
+    size_t print(double, int = 2);
+    size_t print(const Printable&);
+    size_t print(struct tm * timeinfo, const char * format = NULL);
 
-        size_t println(const __FlashStringHelper *);
-        size_t println(const String &s);
-        size_t println(const char[]);
-        size_t println(char);
-        size_t println(unsigned char, int = DEC);
-        size_t println(int, int = DEC);
-        size_t println(unsigned int, int = DEC);
-        size_t println(long, int = DEC);
-        size_t println(unsigned long, int = DEC);
-        size_t println(double, int = 2);
-        size_t println(const Printable&);
-        size_t println(void);
-
-        virtual void flush() { /* Empty implementation for backward compatibility */ }
+    size_t println(const __FlashStringHelper *);
+    size_t println(const String &s);
+    size_t println(const char[]);
+    size_t println(char);
+    size_t println(unsigned char, int = DEC);
+    size_t println(int, int = DEC);
+    size_t println(unsigned int, int = DEC);
+    size_t println(long, int = DEC);
+    size_t println(unsigned long, int = DEC);
+    size_t println(long long, int = DEC);
+    size_t println(unsigned long long, int = DEC);
+    size_t println(double, int = 2);
+    size_t println(const Printable&);
+    size_t println(struct tm * timeinfo, const char * format = NULL);
+    size_t println(void);
 };
 
 #endif
