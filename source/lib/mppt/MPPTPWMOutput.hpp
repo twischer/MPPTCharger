e491c4abd07a3ef45be0a7d1848931f2810ae4b1
Timo Wischer <twischer@freenet.de>
2021-05-12 17:09:45 +0200

    fixup! rpi: Use MPPT PWM output implementation

--- source/lib/mppt/MPPTPWMOutput.hpp
+++ source/lib/mppt/MPPTPWMOutput.hpp
@@ -10,13 +10,18 @@ private:
 
 public:
 	MPPTPWMOutput(const uint8_t outPin) : outPin(outPin) {
+		pwmSetMode(PWM_MODE_MS);
+		/* pwmFrequency in Hz = 19.2e6 Hz / pwmClock / pwmRange */
+		// TODO set frequency lower than XL4016 internal frequency
+		//pwmSetClock() seems to go from 2 to 4095
+		pwmSetRange(maxValue);
 		/* inverted logic: high value reduces PWM output voltage of TL494 */
-		pwmWrite(outPin, maxValue);
+		pwmWrite( outPin, maxValue);
+		pinMode(outPin, PWM_OUTPUT);
 	}
 
 	virtual void write(uint8_t value) override {
 		/* inverted logic */
-		// TODO check range of wiring PI PWM
 		pwmWrite(outPin, maxValue - value);
 	}
 };
